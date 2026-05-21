629fb3b2169aa3ab4418cc21660e9260cffc970f
sobir-swe <sobirjon.swe@gmail.com>
2026-05-22 04:25:21 +0500

    basic: magic constants

---
+++ php/Basic/magic-constants.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Magic Constants - PHP'da maxsus ma'noga ega bo'lgan konstantalar. Ular doimiy qiymatga ega va PHP tomonidan avtomatik
+ * ravishda aniqlanadi. Magic Constants PHP kodining turli qismlarida ishlatiladi va ular kodning kontekstiga qarab
+ * o'zgaradi. Quyida PHP'da mavjud bo'lgan ba'zi Magic Constants keltirilgan:
+ * __LINE__ - joriy kod qatorining raqamini ifodalaydi.
+ * __FILE__ - joriy faylning to'liq yo'lini ifodalaydi.
+ * __DIR__ - joriy faylning katalogining to'liq yo'lini ifodalaydi.
+ * __FUNCTION__ - joriy funksiyaning nomini ifodalaydi.
+ * __CLASS__ - joriy sinfning nomini ifodalaydi.
+ * __METHOD__ - joriy metodning nomini ifodalaydi.
+ * __NAMESPACE__ - joriy namespace'ning nomini ifodalaydi.
+ */
