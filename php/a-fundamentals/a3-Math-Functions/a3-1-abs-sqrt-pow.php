37a6017bb206e11b5fee51ae33c9d559d7f037f4
sobir-swe <sobirjon.swe@gmail.com>
2026-06-06 22:15:53 +0500

    math functions: pi, abs, sqrt, pow

---
+++ php/a-fundamentals/a3-Math-Functions/a3-1-abs-sqrt-pow.php
@@ -0,0 +1,7 @@
+<?php
+
+echo pi() . "\n";  // PI raqamini chiqazib beradi
+echo sqrt(9) . "\n";
+echo pow(9, 2) . "\n";
+echo abs(-9) . "\n";    // abs() funksiyasi sonning mutlaq qiymatini qaytaradi, ya'ni manfiy sonni musbatga aylantiradi. abs(-9) ni chaqirsak ham natija 9 bo'ladi, abs(9) ni chaqirsak ham natija 9 bo'ladi, abs(0) ni chaqirsak ham natija 0 bo'ladi.
+
